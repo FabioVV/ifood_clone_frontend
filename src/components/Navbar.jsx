5b7a2a4b4e96b4492ec1e0caac27ebba3440e8d4
Fábio Varela <fabio-varela@protonmail.com>
2024-02-27 00:07:23 -0300

    v

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -22,123 +22,57 @@ function Navbar() {
     
     return (
         <div style={{padding:'18px'}} className="navbar bg-base-100 sticky top-0 z-10 ">
+            <div className="flex-1">
+                <NavLink className='btn btn-ghost text-xl' to="/">byteFood</NavLink>
+            </div>
+            
+            
             { getCurrentUserToken() ? (
                 <>
-                    <div className="navbar-start">
-                        <div className="dropdown">
-                            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-                                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-                            </div>
-                            <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                                <li><a>Item 1</a></li>
-                                <li>
-                                <a>Parent</a>
-                                <ul className="p-2">
-                                    <li><a>Submenu 1</a></li>
-                                    <li><a>Submenu 2</a></li>
-                                </ul> 
-                                </li>
-                                <li><a>Item 3</a></li>
-                            </ul>
-
-                        </div>
-                        
-
+                    <div className="flex-none">
                         <ul className="menu menu-horizontal px-1">
 
-                            <li>
-                                <a>
-                                    <NavLink className="btn btn-ghost text-4xl"  to="/">Bytefood</NavLink>
-                                </a>
-                            </li>
+                            
+                            <li><a>Link</a></li>
+
 
                             <li>
-                                <a>
-                                    <NavLink className="" >teste</NavLink>
-                                </a>
-                            </li>                            <li>
-                                <a>
-                                    <NavLink className="" >teste</NavLink>
-                                </a>
-                            </li>                            <li>
-                                <a>
-                                    <NavLink className="" >teste</NavLink>
-                                </a>
-                            </li>                            <li>
-                                <a>
-                                    <NavLink className="" >teste</NavLink>
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
-                    <div className="navbar-center hidden lg:flex">
-                        <ul className="menu menu-horizontal px-1">
-                            {/* <li><NavLink  className=""  to="/restaurantes">Restaurantes</NavLink></li> */}
-                            {/* <li>
                                 <details>
-                                <summary>Parent</summary>
-
-                                <ul className="p-2">
-                                    <li><a>Submenu 1</a></li>
-                                    <li><a>Submenu 2</a></li>
+                                    <summary>
+                                        Parent
+                                    </summary>
+                                <ul className="p-2 bg-base-100 rounded-t-none">
+                                    <li><a>Link 1</a></li>
+                                    <li><a>Link 2</a></li>
                                 </ul>
-
                                 </details>
-                            </li> */}
-                            <li>
-                                <a>
-                                    <NavLink className="" >Inicio</NavLink>
-                                </a>
-                            </li>
-
-                            <li>
-                                <a>
-                                    <NavLink className="" >teste</NavLink>
-                                </a>
-                            </li>                            <li>
-                                <a>
-                                    <NavLink className="" >teste</NavLink>
-                                </a>
-                            </li>                            <li>
-                                <a>
-                                    <NavLink className="" >teste</NavLink>
-                                </a>
-                            </li>                            <li>
-                                <a>
-                                    <NavLink className="" >teste</NavLink>
-                                </a>
                             </li>
                         </ul>
-                    </div>
-                    <div className="navbar-end">
-                        <button className="btn" onClick={() => SignOut()}>Sair</button>
                     </div>
                 </> 
         
             ):(
                 <>
-                    <div className="navbar-start justify-center">
-                        <div className="dropdown">
-                            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-                                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-                            </div>
-                            {/* <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                                <li><a>Item 1</a></li>
-                                <li>
-                                <a>Parent</a>
-                                <ul className="p-2">
-                                    <li><a>Submenu 1</a></li>
-                                    <li><a>Submenu 2</a></li>
+                    <div className="flex-none">
+                        <ul className="menu menu-horizontal px-1">
+
+                            <li><NavLink to="/register">Criar conta</NavLink></li>
+                            <li><NavLink to="/login">Login</NavLink></li>
+
+                            
+                            
+                            <li>
+                                <details>
+                                    <summary>
+                                        Parent
+                                    </summary>
+                                <ul className="p-2 bg-base-100 rounded-t-none">
+                                    <li><a>Link 1</a></li>
+                                    <li><a>Link 2</a></li>
                                 </ul>
-                                </li>
-                                <li><a>Item 3</a></li>
-                            </ul> */}
-                        </div>
-                        <NavLink className="btn btn-ghost text-4xl"  to="/">Bytefood</NavLink>
-                    </div>
-                    <div className="navbar-end justify-center">
-                        <NavLink className="btn mr-5" to="/register">Criar conta</NavLink>
-                        <NavLink className="btn" to="/login">Login</NavLink>
+                                </details>
+                            </li>
+                        </ul>
                     </div>
                 </>
             )
