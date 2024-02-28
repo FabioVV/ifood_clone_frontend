8a2db972beae45cdb3c8f0600f280a69e6f0e85c
Fábio Varela <fabio-varela@protonmail.com>
2024-02-27 23:13:30 -0300

    trying new navbars

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 import { NavLink, useNavigate  } from 'react-router-dom'
 import { getCurrentUserToken, getCurrentUser, clearLocalStorage } from '../pages/auth/utils'
 import { googleLogout } from '@react-oauth/google';
-
+import logo1 from '../public/img/logo1.png'
+import logo2 from '../public/img/logo2.png'
+import logo3 from '../public/img/logo3.png'
 
 
 function Navbar() {
@@ -22,57 +24,82 @@ function Navbar() {
     
     return (
         <div style={{padding:'18px'}} className="navbar bg-base-100 sticky top-0 z-10 ">
-            <div className="flex-1">
-                <NavLink className='btn btn-ghost text-xl' to="/">byteFood</NavLink>
-            </div>
-            
-            
+
             { getCurrentUserToken() ? (
                 <>
-                    <div className="flex-none">
+                    <div className="navbar-start">
+                        <div className="dropdown">
+                        <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                        </div>
+                        <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                            <li><a>Item 1</a></li>
+                            <li>
+                            <a>Parent</a>
+                            <ul className="p-2">
+                                <li><a>Submenu 1</a></li>
+                                <li><a>Submenu 2</a></li>
+                            </ul>
+                            </li>
+                            <li><a>Item 3</a></li>
+                        </ul>
+                        </div>
+                        <NavLink className="btn btn-ghost text-xl" to="/">byteFood</NavLink>
+                    </div>
+                    <div className="navbar-center hidden lg:flex">
                         <ul className="menu menu-horizontal px-1">
 
-                            
-                            <li><a>Link</a></li>
+                            <li><a>Something in the way...</a></li>
+                            <li><a>Wait till their judgement day comes, yeah!</a></li>
 
-
-                            <li>
-                                <details>
-                                    <summary>
-                                        Parent
-                                    </summary>
-                                <ul className="p-2 bg-base-100 rounded-t-none">
-                                    <li><a>Link 1</a></li>
-                                    <li><a>Link 2</a></li>
-                                </ul>
-                                </details>
-                            </li>
                         </ul>
+                    </div>
+                    <div className="navbar-end">
+                        <button className="btn" onClick={() => SignOut()}>Sair</button>
                     </div>
                 </> 
         
             ):(
-                <>
-                    <div className="flex-none">
+                <>                                
+                    <div className="navbar-start">
+                        <div className="dropdown">
+                            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                            </div>
+                            {/* DROPDOWN MENU FOR UNLOGGED USERS */}
+
+                            {/* <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                                <li><a>Item 1</a></li>
+                                <li>
+                                    <a>Parent</a>
+                                    <ul className="p-2">
+
+                                        <li><a>Lord of this world!</a></li>
+                                        <li><a>So you think you can tell heaven from hell?</a></li>
+
+                                    </ul>
+                                </li>
+                                <li><a>Item 3</a></li>
+                            </ul> */}
+                        </div>
+                        <div style={{maxWidth:'85px'}}>
+                            
+                            <NavLink  to="/"><img width={350} height={50} src={logo3}/></NavLink>
+                       
+                        </div>
+
+                    </div>
+                    <div className="navbar-center hidden lg:flex">
                         <ul className="menu menu-horizontal px-1">
 
-                            <li><NavLink to="/register">Criar conta</NavLink></li>
-                            <li><NavLink to="/login">Login</NavLink></li>
+                            <li><a>Lord of this world!</a></li>
+                            <li><a>So you think you can tell heaven from hell?</a></li>
 
-                            
-                            
-                            <li>
-                                <details>
-                                    <summary>
-                                        Parent
-                                    </summary>
-                                <ul className="p-2 bg-base-100 rounded-t-none">
-                                    <li><a>Link 1</a></li>
-                                    <li><a>Link 2</a></li>
-                                </ul>
-                                </details>
-                            </li>
                         </ul>
+                    </div>
+                    <div  className="navbar-end gap-3">
+                        <NavLink className='btn' to="/register">Criar conta</NavLink>
+                        <NavLink className='btn' to="/login">Login</NavLink>
                     </div>
                 </>
             )
