8a4df80a894ca0ee0fb37fa3d6fc2e4842dbaded
FabioVV <fabio-varela@protonmail.com>
2024-03-14 10:24:13 -0300

    finishin publicity

--- src/pages/users/edit/Credentials.jsx
+++ src/pages/users/edit/Credentials.jsx
@@ -9,66 +9,67 @@ function Credentials() {
   let navigate = useNavigate();
 
 
-
   return (
     <DefaultPage>
-        <div className='container-user-info'>
-          <ul className='ul-user'>
+      
+      <div className='container-user-info'>
+        <ul className='ul-user'>
 
-              <li style={{marginBottom:'2.5rem'}}>
-                <div className='user-info-edit'>
-                  <strong>
-                    <h1 className='user_data_title' >Meus dados</h1>
-                    <span style={{color: '#717171'}}>Veja aqui suas informações de acesso à conta byteFood</span>
-                  </strong>
-                </div>
-              </li>
+            <li style={{marginBottom:'2.5rem'}}>
+              <div className='user-info-edit'>
+                <strong>
+                  <h1 className='user_data_title' >Meus dados</h1>
+                  <span style={{color: '#717171'}}>Veja aqui suas informações de acesso à conta byteFood</span>
+                </strong>
+              </div>
+            </li>
 
-              <li className='data-user'>
-                <div className='user-info-edit'>
-                  <strong className='user_list_data_title'>
-                    Email
-                  </strong>
-                  <span className='user-info-data'>
-                    {User?.email}
-                  </span>
-                  <span className='user-info-data'>
-                    <span style={{color:'green'}}>Confirmado</span> em {User?.email_confirmed_in ? User?.email_confirmed_in: User?.created_at}
-                  </span>
-                </div>
-              </li>
+            <li className='data-user'>
+              <div className='user-info-edit'>
+                <strong className='user_list_data_title'>
+                  Email
+                </strong>
+                <span className='user-info-data'>
+                  {User?.email}
+                </span>
+                <span className='user-info-data'>
+                  <span style={{color:'green'}}>Confirmado</span> em {User?.email_confirmed_in ? User?.email_confirmed_in: User?.created_at}
+                </span>
+              </div>
+            </li>
 
-              <li className='data-user'>
-                <div className='user-info-edit'>
-                  <strong className='user_list_data_title'>
-                    Celular
-                  </strong>
-                  <span className='user-info-data'>
-                    {User?.phone}
-                  </span>
-                  <span className='user-info-data'>
+            <li className='data-user'>
+              <div className='user-info-edit'>
+                <strong className='user_list_data_title'>
+                  Celular
+                </strong>
+                <span className='user-info-data'>
+                  {User?.phone}
+                </span>
+                <span className='user-info-data'>
 
-                    {User?.phone_confirmed_in ? 
-                        <>
-                            <span style={{color:'green'}}>Confirmado</span> <span>em {User?.phone_confirmed_in}</span>
-                        </>
-                    :
-                        <>
-                            <span style={{color:'salmon'}}>Não Confirmado</span> 
-                        </>
-                    }
-                  </span>
+                  {User?.phone_confirmed_in ? 
+                      <>
+                          <span style={{color:'green'}}>Confirmado</span> <span>em {User?.phone_confirmed_in}</span>
+                      </>
+                  :
+                      <>
+                          <span style={{color:'salmon'}}>Não Confirmado</span> 
+                      </>
+                  }
+                </span>
 
-                    <button style={{marginTop:'2rem'}} type='button' onClick={() => {navigate('/minha-conta')}} className="btn btn-outline">
-                            
-                       Voltar
+                  <button style={{marginTop:'2rem'}} type='button' onClick={() => {navigate('/minha-conta')}} className="btn btn-outline">
+                          
+                      Voltar
 
-                    </button>
-                </div>
-              </li>
+                  </button>
+              </div>
+            </li>
 
-          </ul>
-        </div>
+        </ul>
+      </div>
+
     </DefaultPage>
   )
 }
--- src/pages/users/edit/publicity.jsx
+++ src/pages/users/edit/publicity.jsx
@@ -1,10 +1,60 @@
 import React from 'react'
 import DefaultPage from '../../../components/DefaultPage'
+import { useNavigate } from "react-router-dom";
 
-function publicity() {
+
+function Publicity() {
+
+  let navigate = useNavigate();
+
+
   return (
-    <DefaultPage>publicity</DefaultPage>
+    <DefaultPage>
+
+      <div className='container-user-info' style={{width:'60ch', margin:'24px auto'}}>
+        <ul className='ul-user'>
+
+            <li style={{marginBottom:'2.5rem'}}>
+              <div className='user-info-edit'>
+                <strong>
+                  <h1 className='user_data_title' >Escolha se você quer receber anúncios fora do iFood</h1>
+                  <span style={{color: '#717171'}}>
+                    Os anúncios são apresentados em sites e apps de terceiros que utilizam o 
+                    nosso serviço de publicidade. O anunciante define seu público-alvo, 
+                    sem acesso a seus dados pessoais.
+                  </span>
+                </strong>
+              </div>
+            </li>
+
+
+
+            <li className='data-user'>
+              <div className='user-info-edit'>
+
+                <strong className='user_list_data_title'>
+                  <div className="form-control">
+                    <label className="label cursor-pointer">
+                      <span className="label-text">Quero receber anúncios de terceiros</span> 
+                      <input type="checkbox" className="toggle" name='' />
+                    </label>
+                  </div>
+                </strong>
+
+                <span className='user-info-data'>
+
+                </span>
+                
+              </div>
+            </li>
+            <button style={{marginTop:'2rem'}} type='button' onClick={() => {navigate('/minha-conta')}} className="btn btn-outline">      
+              Voltar
+            </button>
+        </ul>
+      </div>
+
+    </DefaultPage>
   )
 }
 
-export default publicity
+export default Publicity
