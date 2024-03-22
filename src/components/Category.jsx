561fffb867158109bc614b6aaca9f3a438c3e0a8
FabioVV <fabio-varela@protonmail.com>
2024-03-22 16:12:23 -0300

    WUP

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -10,6 +10,7 @@ function Category({category, HandleFetch}) {
     <div id='category' >
 
       <div id='image-container' className='rounded-lg  overflow-hidden'>
+        {/* FAZER UM IF AQUI PARA CHECAR SE O LINK EXISTE {category?.image ?}, PARA ELE NAO FAZER REQUISIÇÕES PROS SERVIDORES TODA VEZ */}
         <img width={95} height={95} src={`http://localhost:8000${category?.image ? category?.image :'/media/_default/default.jpeg'}`} alt={`Imagem da categoria ${category?.name}`} className='rounded-lg overflow-hidden'/>
       </div>
 
