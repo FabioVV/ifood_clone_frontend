ae37e3759415a074f79f86973067f89c8af791d5
Fabio <fabio-varela@protonmail.com>
2024-05-06 19:34:22 -0300

    WIP cards

--- src/pages/payment/Index.jsx
+++ src/pages/payment/Index.jsx
@@ -8,13 +8,26 @@ function IndexPayment() {
   return (
     <DefaultPage>
         {newCard ? 
-        "aaa"
+          <section className='payment-index-section'>
+            <h1 className='payment-title'>Escolha um meio de pagamento</h1>
+            <div className='payment-types'>
+              <div>
+                <span><i class="fa-solid fa-credit-card"></i></span>
+                <span>Crédito</span>
+              </div>
+              <div>
+                <span><i class="fa-regular fa-credit-card"></i></span>
+                <span>Débito</span>
+              </div>
+            </div>
+
+          </section>
         :
             <section className='payment-index-section'>
                 <h1 className='payment-title'>Formas de pagamento</h1>
                 <h2 className='payment-subtitle'>Adicione um cartão bytefood</h2>
                 <p className='payment-text'>É prático, seguro e você não perde nenhum minuto a mais quando seu pedido chegar.</p>
-                <button onClick={()=>{setNewCard(!newCard)}} type="button" className="btn text-black-600">Adicionar novo cartão</button>
+                <button onClick={()=>{setNewCard(!newCard)}} type="button" className="btn btn-active btn-primary">Adicionar novo cartão</button>
             </section>
         }
 
